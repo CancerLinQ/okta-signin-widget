abfb662551841c300a98df004aab0faa4157d73d
Poonam Shah <poonam.shah1985@gmail.com>
2016-10-12 14:21:18 -0700

    Adding back to sign in footer
    Adding back to sign in footer

--- src/UserCreationController.js
+++ src/UserCreationController.js
@@ -15,10 +15,10 @@ define([
   'util/FormController',
   'util/FormType',
   'util/ValidationUtil',
-  'views/shared/FooterSignout',
+  'views/enroll-factors/BackToSigninFooter',
   'views/shared/TextBox'
 ],
-function (Okta, FormController, FormType, ValidationUtil, FooterSignout, TextBox) {
+function (Okta, FormController, FormType, ValidationUtil, BackToSigninFooter, TextBox) {
 
   var _ = Okta._;
 
@@ -58,10 +58,8 @@ function (Okta, FormController, FormType, ValidationUtil, FooterSignout, TextBox
             token: gup('recoveryToken', window.location.search)
           })
           .then( function(transaction) {
+            console.log(JSON.stringify(transaction));
             // Need to display errors if they are passed in.
-            // signin/created-user is set as a stateless page because the signup
-            // function is a conglomerate customized for CLQ and does not represent
-            // one of the defined Okta authorization states.
             self.options.appState.trigger('navigate', 'signin/created-user');
           })
           .fail(function() {
@@ -164,7 +162,7 @@ function (Okta, FormController, FormType, ValidationUtil, FooterSignout, TextBox
         ];
       }
     },
-    Footer: FooterSignout,
+    Footer: BackToSigninFooter,
 
     initialize: function () {
       this.listenTo(this.form, 'save', function () {
@@ -203,68 +201,7 @@ function (Okta, FormController, FormType, ValidationUtil, FooterSignout, TextBox
           return http.get(self.settings.getAuthClient(), 
             self.settings.attributes.oktaUrl + '/api/v1/users/00u829stnwnkWBj340h7/factors/questions')
         }
-        // return factor.questions();
-        return [{
-    "question": "disliked_food",
-    "questionText": "What is the food you least liked as a child?"
-  }, {
-    "question": "name_of_first_plush_toy",
-    "questionText": "What is the name of your first stuffed animal?"
-  }, {
-    "question": "first_award",
-    "questionText": "What did you earn your first medal or award for?"
-  }, {
-    "question": "favorite_security_question",
-    "questionText": "What is your favorite security question?"
-  }, {
-    "question": "favorite_toy",
-    "questionText": "What is the toy\/stuffed animal you liked the most as a kid?"
-  }, {
-    "question": "first_computer_game",
-    "questionText": "What was the first computer game you played?"
-  }, {
-    "question": "favorite_movie_quote",
-    "questionText": "What is your favorite movie quote?"
-  }, {
-    "question": "first_sports_team_mascot",
-    "questionText": "What was the mascot of the first sports team you played on?"
-  }, {
-    "question": "first_music_purchase",
-    "questionText": "What music album or song did you first purchase?"
-  }, {
-    "question": "favorite_art_piece",
-    "questionText": "What is your favorite piece of art?"
-  }, {
-    "question": "grandmother_favorite_desert",
-    "questionText": "What was your grandmother's favorite dessert?"
-  }, {
-    "question": "first_thing_cooked",
-    "questionText": "What was the first thing you learned to cook?"
-  }, {
-    "question": "childhood_dream_job",
-    "questionText": "What was your dream job as a child?"
-  }, {
-    "question": "first_kiss_location",
-    "questionText": "Where did you have your first kiss?"
-  }, {
-    "question": "place_where_significant_other_was_met",
-    "questionText": "Where did you meet your spouse\/significant other?"
-  }, {
-    "question": "favorite_vacation_location",
-    "questionText": "Where did you go for your favorite vacation?"
-  }, {
-    "question": "new_years_two_thousand",
-    "questionText": "Where were you on New Year's Eve in the year 2000?"
-  }, {
-    "question": "favorite_speaker_actor",
-    "questionText": "Who is your favorite speaker\/orator?"
-  }, {
-    "question": "favorite_book_movie_character",
-    "questionText": "Who is your favorite book\/movie character?"
-  }, {
-    "question": "favorite_sports_player",
-    "questionText": "Who is your favorite sports player?"
-  }];
+        return factor.questions();
       })
       .then(function(questionRes) {
         var questions = {};
