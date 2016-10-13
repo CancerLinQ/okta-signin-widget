6dc07116999dea8795c82f27d106ff72ad238af2
Ryan Ma <ryan.ma@cancerlinq.org>
2016-10-13 09:28:45 -0700

    Center the widget

--- src/util/CLQText.js
+++ src/util/CLQText.js
@@ -1,5 +1,6 @@
 var CLQText = (function() {
   return {
+    // CLQ custom text
     'error.auth.lockedOut.selfUnlock': 'Your account was locked due to excessive attempts. To protect your security, please unlock your account',
     'error.expired.accountUnlockToken': 'Session expired, please request a new account unlock',
     'error.expired.passwordResetToken': 'Session expired, please request a new password reset',
@@ -9,6 +10,15 @@ var CLQText = (function() {
     'usercreation.failure': 'There was an issue during creation, please contact your admin',
     'usercreationsuccess.title': 'User successfully created',
     'usercreationsuccess.subtitle': 'Please return and log in to access CLQ'
+
+    // Over-write existing widget text
+    /*
+    enroll.choices.description = Your company requires multifactor authentication to add an additional layer of security when signing into your Okta account
+    factor.u2f.description = Use a Universal 2nd Factor (U2F) security key to sign on to Okta.
+    password.reset.title = Reset your Okta password
+    password.expired.title = Your Okta password has expired
+    password.expiring.subtitle = When password expires you may be locked out of Okta Mobile, mobile email, and other services.
+     */
   }
 })();
 
